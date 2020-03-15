6f1d4f5c60e419e8430ba48ac6c932dc2f5c29f6
Arthur Coruble <corublearthur@gmail.com>
2020-03-15 10:07:00 +0100

    Re-init project with Typescript starter

--- config-overrides.js
+++
@@ -1,2 +0,0 @@
-const { useBabelRc, override, addDecoratorsLegacy } = require("customize-cra");
-module.exports = override(useBabelRc(), addDecoratorsLegacy());
--- src/organisms/index.js
+++
@@ -1,3 +0,0 @@
-export { default as TimerCounter } from "./TimerCounter";
-export { default as StopWatchCounter } from "./StopWatchCounter";
-export { default as MenuBar } from "./MenuBar";
